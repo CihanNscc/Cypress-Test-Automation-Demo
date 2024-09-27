3d5cb490b399f01cf54d9658854105e26cd59874
CihanNscc <alcincihan@gmail.com>
2024-09-27 00:05:25 -0300

    sample test added, css corrected for next.js

---
+++ cypress/e2e/pen-card-test.cy.ts
@@ -0,0 +1,5 @@
+describe("Pen Card", () => {
+  it("Visit site", () => {
+    cy.visit("http://localhost:3000/");
+  });
+});
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ export default function Home() {
   return (
     <div className="w-full h-screen flex items-center justify-center">
       <div className="max-w-sm text-center space-y-10">
-        <h1 className="text-3xl font-bold">Pen Shop</h1>
+        <h1 className="text-2xl font-bold text-zinc-100">
+          Cypress Demo Pen Shop
+        </h1>
         <PenCard />
-        <p className="text-sm text-gray-500">
+        <p className="text-sm text-zinc-300">
           This is a basic UI for a Cypress demo.
         </p>
       </div>
